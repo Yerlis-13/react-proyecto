8ac4ace4f7a9b9a67d3b6ead739949d569b4ed9e
Yerlis-13 <yerlisarrieta13@gmail.com>
2023-11-27 16:52:15 -0500

    repositorio de reat

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import  Message from "./message";
+
+function App() {
+  return (
+  <div className='App'>
+    <h1>Hola Desde Reacy</h1>
+    <Message name={'duviel'}/>
+  </div>
+  );
+}
+
+export default App;
